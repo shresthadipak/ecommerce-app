a992b12ee7601a3d6a20f35bdb8aec693ecbbcf2
Dipak Shrestha <shresthadipak2049@gmail.com>
2023-04-15 13:21:08 +0545

    Mysql: Database connection

---
+++ src/main/java/com/apps/ecom/entities/Product.java
@@ -0,0 +1,29 @@
+package com.apps.ecom.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@Setter
+@Getter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer productId;
+
+    @Column(name="product_title", length = 100, nullable = false)
+    private String title;
+
+    private Integer quantity;
+
+    private Long price;
+
+    @Column(length = 10000)
+    private String description;
+}
